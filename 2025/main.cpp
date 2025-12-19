c0a4509052661ab826f3b14849987ab8544635a6
EMEA\jan-niklas.schneider <jan-niklas.schneider@thermofisher.com>
2025-12-19 10:57:56 +0100

    uncomment debug

--- 2025/main.cpp
+++ 2025/main.cpp
@@ -6,31 +6,31 @@ void testHasRepeatingPattern();
 
 int main() {
 	// === UNCOMMENT TO RUN TESTS ===
-	testHasRepeatingPattern();
-	return 0;
+	/*testHasRepeatingPattern();
+	return 0;*/
 
 	// === UNCOMMENT TO RUN SOLUTIONS ===
 
 
-	//int day;
-	//std::cout << "Advent of Code 2025" << std::endl;
-	//std::cout << "Enter day number (1-25): ";
-	////std::cin >> day;
-	////std::cin.ignore(); // Clear newline from buffer
-	//day = 2;
+	int day;
+	std::cout << "Advent of Code 2025" << std::endl;
+	std::cout << "Enter day number (1-25): ";
+	//std::cin >> day;
+	//std::cin.ignore(); // Clear newline from buffer
+	day = 2;
 
-	//switch (day) {
-	//case 1:
-	//	day1();
-	//	break;
-	//case 2:
-	//	day2();
-	//	break;
-	//// Add more cases as you complete more days
-	//default:
-	//	std::cout << "Day " << day << " not implemented yet!" << std::endl;
-	//	break;
-	//}
+	switch (day) {
+	case 1:
+		day1();
+		break;
+	case 2:
+		day2();
+		break;
+	// Add more cases as you complete more days
+	default:
+		std::cout << "Day " << day << " not implemented yet!" << std::endl;
+		break;
+	}
 
 	return 0;
 }
